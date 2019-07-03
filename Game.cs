0bab544b1438fd5ba749993b498b68d395b421ba
Jeffrey Caldwell <bitbrainresearch@gmail.com>
2019-07-03 06:58:16 -0400

    Initial Commit

---
+++ Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TelnetTest
+{
+
+    
+    class Game
+    {
+        public SystemState CurrentSystemState;
+
+        public string name = "";
+        public string password = "";
+
+        public Game()
+        {
+            CurrentSystemState = SystemState.NAME;
+        }
+
+        public string CheckForName(string input)
+        {
+            Regex nameRegex = new Regex(@"Name:");
+            
+            Match matchName = nameRegex.Match(input);
+            
+            if (matchName.Success)
+            {
+                return name;
+            } else
+            {
+                return "";
+            }
+        }
+
+        public string CheckForPass(string input)
+        {
+            Regex passRegex = new Regex(@"Password:");
+            Match matchPass = passRegex.Match(input);
+
+            if (matchPass.Success)
+            {
+                return password;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public bool HandleCharacterCreation()
+        {
+
+            return false;
+        }
+
+        public enum SystemState
+        {
+            NAME,
+            PASS,
+            NEUTRAL,
+            COMBAT,
+            EXPLORE,
+            SHOPPING
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using MinimalisticTelnet;
+using System;
+
+namespace TelnetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelnetConnection tc = new TelnetConnection("", 0);
+            Game game = new Game();
+
+            string buffer = "";
+            string output = "";
+            Console.WriteLine("Case Name");
+
+            while(tc.IsConnected)
+            {
+                output = "";
+
+                buffer = tc.Read();
+                Console.Write(buffer);
+                
+
+                switch (game.CurrentSystemState)
+                {
+                    case Game.SystemState.NAME:
+                        
+                        output = game.CheckForName(buffer);
+                        if (output != "")
+                        {
+                            game.CurrentSystemState = Game.SystemState.PASS;
+                            Console.WriteLine("State Pass");
+                        }
+                        break;
+                    case Game.SystemState.PASS:
+                        output = game.CheckForPass(buffer);
+                        if (output != "")
+                        {
+                            game.CurrentSystemState = Game.SystemState.NEUTRAL;
+                            Console.WriteLine("Case Neutral");
+                        }
+                        break;
+                    case Game.SystemState.NEUTRAL:
+                        
+                        break;
+                    case Game.SystemState.EXPLORE:
+                        Console.WriteLine("Case Explore");
+                        break;
+                    case Game.SystemState.COMBAT:
+                        Console.WriteLine("Case Combat");
+                        break;
+                    case Game.SystemState.SHOPPING:
+                        Console.WriteLine("Case Shopping");
+                        break;
+                }
+
+                
+                if(output != "")
+                {
+                    tc.WriteLine(output);
+                }
+                
+                
+            }
+        }
+
+
+
+    }
+}
